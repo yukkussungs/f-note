18319c045cc74c2b86b02bbf9a4e962a06465825
woosung.shin <sws7420@gmail.com>
2018-12-14 23:48:37 +0900

    set react environment and set firebase environment

---
+++ f-note/src/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+const firebase = require("firebase");
+require("firebase/firestore");
+require("dotenv").config();
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+console.log("process.env.APIKEY : ", process.env.APIKEY);
+//set firebase
+firebase.initializeApp({
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
+});
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
